2bf8c3b73b9c4dc76141b7224377adc32a44a181
gw-shinohara <shinohara@globalwalkers.co.jp>
2023-04-14 15:52:08 +0900

    Update

--- docker_build_image.sh
+++ docker_build_image.sh
@@ -6,8 +6,11 @@
 . ./setting
 
 # Building Docker repository
-echo -n "Root user password:" 
+echo -n "user of Root password:" 
 read ROOT_PASS
 
+echo -n "user of $USER_NAME password:"
+read USER_PASS
+
 tty -s && echo
-docker build -t $repository_name:$repository_version $REPOSITORY_DIR --build-arg BASE_IMAGE=$BASE_IMAGE --build-arg TIME_ZONE=$TIME_ZONE --build-arg ROOT_PASS=$ROOT_PASS --build-arg USER_NAME=$USER_NAME --build-arg HOME_DIR=$HOME_DIR
+docker build -t $repository_name:$repository_version $REPOSITORY_DIR --build-arg BASE_IMAGE=$BASE_IMAGE --build-arg TIME_ZONE=$TIME_ZONE --build-arg ROOT_PASS=$ROOT_PASS --build-arg USER_NAME=$USER_NAME --build-arg USER_PASS=$USER_PASS
--- repository/entrypoint.sh
+++ repository/entrypoint.sh
@@ -2,20 +2,15 @@
 
 USER_ID=${LOCAL_UID:-9001}
 GROUP_ID=${LOCAL_GID:-9001}
-USER_NAME=${LOCAL_USER:developer}
-GROUP_NAME=${LOCAL_GROUP:developer}
-HOME_DIR=${LOCAL_HOME}
+USER_NAME=${LOCAL_USER:locker}
+GROUP_NAME=${LOCAL_GROUP:locker}
 
-echo "entrypoint.sh"
 echo "Starting with UID : $USER_ID, GID: $GROUP_ID"
-echo "USER_NAME: ${USER_NAME}"
-echo "GROUP_NAME: ${GROUP_NAME}"
-# adduser --uid ${USER_ID} --home ${HOME_DIR} ${USER_NAME} && chown -R ${USER_NAME} ${HOME_DIR}
-sudo useradd -m $USER_NAME -u $USER_ID -s /bin/bash && sudo chown -R ${USER_NAME} ${HOME_DIR}
 
-echo -n "$USER_NAME New password "
-sudo passwd $USER_NAME
+sudo groupmod -g $GROUP_ID $GROUP_NAME
+sudo usermod -u $USER_ID $USER_NAME
+# sudo usermod -G sudo $USER_NAME
+# sudo find / -user 9999 -exec chown -h $USER_NAME {} \;
+# sudo find / -group 9999 -exec chgrp -h $GROUP_NAME {} \;
 
-sudo usermod -G sudo $USER_NAME
-sudo cat /etc/group | grep $USER_NAME
-su $USER_NAME | echo "id:" | id
+id $USER_NAME
